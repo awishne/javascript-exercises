f66539c8e9c7a054b65029d129fa6433bce9d2f9
Adam Wishne <99109711+awishne@users.noreply.github.com>
2023-08-07 20:11:41 -0500

    Complete Excercises 01 to 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+   let start = first < second ? first : second;
+   let end = first > second ? first : second;
+   let sum = 0
 
+   if (typeof first !== "number" || typeof second !== "number" || first < 0 || second < 0) {
+    return "ERROR";
+   }
+
+   for(i = start; i <= end; i++) {
+    sum += i;
+   }
+
+   return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// write the first number at top, second number at bottom
+// fill in all numbers in between and sum them all
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempConversion) {
+  const celsius =(tempConversion - 32) * 5/9;
+  return Math.round(celsius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempConversion) {
+  const fahrenheit =(tempConversion * 9/5 + 32);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
